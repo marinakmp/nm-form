fd8c656ed099944a66bc39ae1fa6b28e72a3776c
lysandros <lysandros@dnd.solutions>
2020-03-04 13:23:59 +0200

    added responsive, social-icons, favicons

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,15 @@ const sourcemaps = require('gulp-sourcemaps');
 const cssmin = require('gulp-cssmin');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 
 function css() {
 	return src('assets/scss/*.scss')
 	.pipe(sourcemaps.init())
-	.pipe(sass())
+	.pipe(sass())	
+    .pipe(autoprefixer({
+			cascade: false
+	})) 
 	.pipe(sourcemaps.write('.'))
     .pipe(dest('dist/assets/css'))
 }
@@ -16,7 +20,10 @@ function css() {
 function minify() {
 	return src('assets/scss/*.scss')
 	.pipe(sourcemaps.init())
-	.pipe(sass())
+	.pipe(sass())	
+    .pipe(autoprefixer({
+			cascade: false
+	})) 
 	.pipe(cssmin())
 	.pipe(rename({suffix: '.min'}))
 	.pipe(sourcemaps.write('.'))
